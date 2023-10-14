5e39f3c80736e1f514d9892d0e8819b320fe433c
Will <30413278+wcrum@users.noreply.github.com>
2023-10-14 00:53:25 -0600

    A lot

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				rubik: ['Rubik', 'sans'], // 'Rubik' is the font name
-			  },
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik', 'sans'], // 'Rubik' is the font name
+      },
+    },
+  },
+  plugins: [],
 }
