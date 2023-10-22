2d3b6bbeef4fc5e027f8cc080b4cbf4dcabe7757
David Whalen <dwhalen3@jh.edu>
2023-10-22 13:50:15 -0400

    Initial commit for roles page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,11 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      textColor: {
+        white: "#FFF",
+        black: "#000",
+        red: "#F70A0A",
+      },
       fontFamily: {
         rubik: ['Rubik', 'sans'], // 'Rubik' is the font name
       },
