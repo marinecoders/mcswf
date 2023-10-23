c47335fa6b6834bc51380ab1baf0d3a4198cbe3a
Sean Sullivan <seanbsullivan@gmail.com>
2023-10-22 21:42:15 -0400

    cohort file structure + initial code

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,14 +2,27 @@
 import { z, defineCollection } from 'astro:content';
 // 2. Define your collection(s)
 const prereqCollection = defineCollection({
-    type: 'data',
-    schema: z.object({
-        title: z.string(),
-        content: z.string(),
-    })
- });
+  type: 'data',
+  schema: z.object({
+    title: z.string(),
+    content: z.string(),
+  })
+});
+
+const cohortCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string(),
+    content: z.string(),
+    startDate: z.string(),
+    endDate: z.string(),
+  })
+});
+
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'prereqs': prereqCollection,
+  'cohorts': cohortCollection,
+  'prereqs': prereqCollection
 };
