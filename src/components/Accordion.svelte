dcbc1f01bd4280c4fde1691bb342234acf3d9b65
Daniel Fullaway <dfullaway@danielmfullaway.com>
2024-01-10 22:23:16 -0700

    WIP. Mobile improvements.

--- src/components/Accordion.svelte
+++ src/components/Accordion.svelte
@@ -1,22 +1,18 @@
 <script>
-  import RedButton from './buttons/RedButton.svelte'
   import AccordionSectionOpen from './elements/AccordionSectionOpen.svelte'
   import AccordionSectionClosed from  './elements/AccordionSectionClosed.svelte'
   let active = 0;
-  import { tweened } from 'svelte/motion';
-  import { sequence } from 'astro:middleware'
-
 </script>
 
 
 <div class="grid grid-cols-1 md:grid-cols-6 md:h-max-content" >
   <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <div role="button" tabindex="0" class="col-span-1  {active==0 ? 'h-96': 'h-72'} lg:{active==0 ? 'col-span-3': 'col-span-2'} bg-pink-400 lg:h-full" on:click={() => (active=0)}>
+  <div role="button" tabindex="0" class="col-span-1  h-96 lg:{active==0 ? 'col-span-3': 'col-span-2'} bg-pink-400 lg:h-full" on:click={() => (active=0)}>
     Our Product Portfolio
   </div>
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   {#key active}
-  <div role="button" tabindex="0" class="{active==1 ? 'h-96': 'h-32'} col-span-1 lg:row-span-1 md:h-full lg:{active==1 ? 'col-span-2': 'col-span-1'} red-radial-gradient" on:click={() => (active=1)} >
+  <div role="button" tabindex="0" class="h-fit col-span-1 lg:row-span-1 md:h-full lg:{active==1 ? 'col-span-2': 'col-span-1'} red-radial-gradient" on:click={() => (active=1)} >
     <div class="relative h-full lg:border-x lg:border-y-0 border-y">
       {#if active == 1}
       <AccordionSectionOpen 
@@ -27,14 +23,14 @@
       {:else}
       <AccordionSectionClosed 
       title="SPARTA"
-      sequence=1
+      sequence=01
       />
       {/if}
     </div>
   </div>
   {/key}
   <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <div role="button" tabindex=0 class="{active==2 ? 'h-96': 'h-32'} col-span-1 lg:{active==2 ? 'col-span-2': 'col-span-1'} xl:{active==2 ? 'col-span-2': 'col-span-1'} red-radial-gradient" on:click={() => (active=2)}>
+  <div role="button" tabindex=0 class="h-fit col-span-1 lg:{active==2 ? 'col-span-2': 'col-span-1'} xl:{active==2 ? 'col-span-2': 'col-span-1'} red-radial-gradient" on:click={() => (active=2)}>
     <div class="relative h-full lg:border-x lg:border-y-0 border-y">
     {#if active ==2}
     <AccordionSectionOpen 
@@ -45,13 +41,13 @@
     {:else}
     <AccordionSectionClosed 
     title="my Career"
-    sequence=2
+    sequence= 02
     />
     {/if}
     </div>
   </div>
   <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <div role="button" tabindex=0 class="{active==3 ? 'h-96': 'h-32'} col-span-1 lg:{active==3 ? 'col-span-2': 'col-span-1'} xl:{active==3 ? 'col-span-2': 'col-span-1'} red-radial-gradient" on:click={() => (active=3)}>
+  <div role="button" tabindex=0 class="h-fit col-span-1 lg:{active==3 ? 'col-span-2': 'col-span-1'} xl:{active==3 ? 'col-span-2': 'col-span-1'} red-radial-gradient" on:click={() => (active=3)}>
     <div class="relative h-full lg:border-x lg:border-y-0 border-y">
       {#if active ==3}
       <AccordionSectionOpen 
@@ -62,7 +58,7 @@
       {:else}
       <AccordionSectionClosed 
       title="Snap DB"
-      sequence=3
+      sequence=03
       />
       {/if}
     </div>
--- src/components/elements/AccordionSectionOpen.svelte
+++ src/components/elements/AccordionSectionOpen.svelte
@@ -6,15 +6,14 @@
     export let href
 </script>
 
-
-<div class="absolute inset-12 top-4 font-bold block text-xl md:text-2xl lg:text-4xl"> {title} </div>
-<div class="absolute flex inset-12 top-20 text-lg">
+<div class="grid grid-cols-2 h-fit">
+<div class="col-span-2 h-fit text-center font-bold block text-xl md:text-2xl lg:text-4xl"> {title} </div>
+<div class="col-span-2 text-lg">
   {text}
 </div>
-<div class="absolute bottom-1/4 left-1/4 flex w-64">
+<div class="col-span-2 h-fit">
   <RedButton text="LEARN MORE" href={href} />
 </div>
-<div class="absolute justify-between overflow-visible text-white align-bottom itemsfirst-letter:-center items-end bottom-0 inset-x-0" >
-  <div class="absolute items-end text-xl text-left font-menobanner text-mcswf-gold left-10 bottom-10"> {sequence}</div>
-  <div class="absolute mb-2 text-xl font-bold text-end whitespace-nowrap text-mcswf-gold right-10 bottom-10"> - </div>
+  <div class="col-span-1 text-xl text-left font-menobanner text-mcswf-gold left-10 bottom-10"> {sequence}</div>
+  <div class="col-span-1 text-xl font-bold text-end whitespace-nowrap text-mcswf-gold right-10 bottom-10"> - </div>
 </div>
