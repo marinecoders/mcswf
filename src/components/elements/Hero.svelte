a3f37c79e150ab294950f43666df2449227809e9
Scott Pierce-Wrobel <scottpiercew@gmail.com>
2024-04-14 23:51:49 -0400

    Minor fixes found post-MR to bring dev and main features in-line with each other

--- src/components/elements/Hero.svelte
+++ src/components/elements/Hero.svelte
@@ -1,40 +1,35 @@
 <script>
-    import RedButton from '../buttons/RedButton.svelte';
-  
-    export let title;
-    export let content = "";
-    export let link;
-    export let linkText;
-    export let padding = "";
-    export let contentClasses;
-  </script>
-  
-  <div class=
-      {'max-w-[85rem] mx-auto px-4 pt-inherit ' + padding}
-    
-  >
-    <!-- Title -->
-    <div class="max-w-6xl mx-auto mt-5 text-center">
-      <h1 class="block text-4xl font-bold md:text-5xl lg:text-6xl text-mcswf-gold">
-        {title}
-      </h1>
-    </div>
-    <!-- End Title -->
-  
-    <div class="max-w-3xl mx-auto mt-5 text-center">
-      <p id='content' class:list={[
-        "text-lg text-white",
-        contentClasses,
-      ]}>
-        {content}
-      </p>
-    </div>
-  
-    <!-- Buttons -->
-    {#if link}
-      <div class="grid w-full gap-3 mt-8 sm:inline-flex sm:justify-center">
-        <RedButton href={link} text={linkText} />
-      </div>
-    {/if}
-    <!-- End Buttons -->
+  import RedButton from '../buttons/RedButton.svelte'
+
+  export let title
+  export let content = ''
+  export let link
+  export let linkText
+  export let padding = ''
+  export let contentClasses
+  export let titleClasses = 'text-mcswf-gold'
+</script>
+
+<div class={'max-w-[85rem] mx-auto px-4 pt-inherit ' + padding}>
+  <!-- Title -->
+  <div class="max-w-6xl mx-auto mt-5 text-center">
+    <h1 class="block text-4xl font-bold md:text-5xl lg:text-6xl {titleClasses}">
+      {title}
+    </h1>
   </div>
+  <!-- End Title -->
+
+  <div class="max-w-3xl mx-auto mt-5 text-center">
+    <p id="content" class:list={['text-lg text-white', contentClasses]}>
+      {content}
+    </p>
+  </div>
+
+  <!-- Buttons -->
+  {#if link}
+    <div class="grid w-full gap-3 mt-8 sm:inline-flex sm:justify-center">
+      <RedButton href={link} text={linkText} />
+    </div>
+  {/if}
+  <!-- End Buttons -->
+</div>
