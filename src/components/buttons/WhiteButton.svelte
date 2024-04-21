616013dea241c64af4fe22cf2e3b65882ec1a225
Will <30413278+wcrum@users.noreply.github.com>
2024-04-20 21:05:07 -0600

    Update WhiteButton.svelte

--- src/components/buttons/WhiteButton.svelte
+++ src/components/buttons/WhiteButton.svelte
@@ -6,11 +6,13 @@
   export let extras
 </script>
 
-<button
-  type="button"
-  class="flex items-center justify-center px-12 py-2 text-xs font-semibold leading-loose  transition-all bg-white border border-transparent rounded-md shadow-lg text-mcswf-pinstripe hover:ring-2 focus:outline-none focus:ring-2 ring-offset-white focus:ring-red-500 hover:ring-red-500 focus:ring-offset-2 focus:ring-offset-gray-800"
->
-  <a {href}>
+<a href={href} class="flex items-center justify-center">
+
+  <button
+    href=""
+    type="button"
+    class="flex w-full items-center justify-center px-12 py-2 text-xs font-semibold leading-loose transition-all bg-white border border-transparent rounded-md shadow-lg text-mcswf-pinstripe hover:ring-2 focus:outline-none focus:ring-2 ring-offset-white focus:ring-red-500 hover:ring-red-500 focus:ring-offset-2 focus:ring-offset-gray-800"
+  >
     {text}
-  </a>
-</button>
+  </button>
+</a>
