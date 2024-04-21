d8449b05f30334d3b7b4cf87b731460a935d34fe
Daniel Fullaway <dfullaway@danielmfullaway.com>
2024-04-21 14:31:28 -0700

    Update accordion section.
    Set all breakpoints to lg (including mobile width). Adjusted spacing.

--- src/components/accordion/Accordion.svelte
+++ src/components/accordion/Accordion.svelte
@@ -11,7 +11,7 @@
 
   let width = 0;
 
-  $: mobile.set(width < 640);
+  $: mobile.set(width < 1024);
 
   function setActive(section){
     if ($active == parseInt(section)){
@@ -31,21 +31,21 @@
 
 <svelte:window bind:innerWidth={width} />
 
-<div class="grid grid-cols-1 sm:h-5/6" class:sm:grid-cols-6={products.length == 3} class:sm:grid-cols-7={products.length == 4}>
+<div class="grid grid-cols-1 lg:h-5/6" class:lg:grid-cols-6={products.length == 3} class:lg:grid-cols-7={products.length == 4}>
   <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <div role="button" tabindex="0" class="flex flex-col col-span-1 justify-end sm:justify-normal h-96 px-16 sm:px-32 {($active==0 && !$mobile) ? 'col-span-3': ''} {($active!=0 && !$mobile) ? 'col-span-2': ''} bg-[url('/images/shaking_hands.png')] bg-auto bg-no-repeat bg-center sm:h-full" on:click={() => setActive(0)}>
+  <div role="button" tabindex="0" class="flex flex-col col-span-1 justify-end lg:justify-normal h-96 px-16 lg:px-32 {($active==0 && !$mobile) ? 'col-span-3': ''} {($active!=0 && !$mobile) ? 'col-span-2': ''} bg-[url('/images/shaking_hands.png')] bg-auto bg-no-repeat bg-center lg:h-full" on:click={() => setActive(0)}>
     <div class="h-1/4"></div>
-      <div class="py-8 text-mcswf-gold font-bold text-center font-expressway text-xl sm:text-4xl">
+      <div class="py-8 text-mcswf-gold font-bold text-center font-expressway text-xl lg:text-4xl">
         OUR PRODUCT PORTFOLIO
       </div>
-      <div class="text-lg sm:text-2xl font-expressway text-center py-8">
+      <div class="text-base lg:text-2xl font-expressway text-center py-8">
         Real solutions to real problems - Created for Marines, by Marines at the Marine Corps Software Factory
       </div>
     </div>
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   {#each products as product}
-    <div role="button" tabindex="0" class="h-fit sm:h-full {($active==product.data.order && !$mobile) ? 'col-span-2': 'col-span-1'} red-radial-gradient" on:click={() => setActive(product.data.order)} >
-      <div class="relative h-full sm:border-x sm:border-y-0 border-y">
+    <div role="button" tabindex="0" class="h-fit lg:h-full {($active==product.data.order && !$mobile) ? 'col-span-2': 'col-span-1'} red-radial-gradient" on:click={() => setActive(product.data.order)} >
+      <div class="relative h-full lg:border-x lg:border-y-0 border-y">
         <AccordionSection
           text={product.data.description}
           title={product.data.product}
