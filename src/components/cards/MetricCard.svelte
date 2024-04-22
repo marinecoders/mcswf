aedf4ae4e84b81a2572b4877a2771a2490f17b6a
Daniel Fullaway <dfullaway@danielmfullaway.com>
2024-04-21 20:07:32 -0700

    Tweak to formating.

--- src/components/cards/MetricCard.svelte
+++ src/components/cards/MetricCard.svelte
@@ -1,13 +1,13 @@
 <script>
-    export let icon;
-    export let value;
-    export let metric;
-  </script>
+  export let icon;
+  export let value;
+  export let metric;
+</script>
   
-  <div class="rounded border-2 h-80 w-52 p-4 m-auto">
-    <img class="mx-auto h-24" src={icon} alt="Image Description" />
-    <div class="flex flex-col mt-2 sm:mt-4 justify-center">
-      <h3 class="text-6xl font-extrabold text-white font-menobanner">{value}</h3>
-      <p class="mx-auto w-5/6 content-center text-lg font-semibold text-mcswf-gold">{metric}</p>
-    </div>
+<div class="rounded-lg flex h-72 w-56 border-2 p-4 border-white">
+  <div class="w-7/8 m-auto align-middle grid gap-4">
+    <img class="mx-auto max-h-24" src={icon} alt="Description" />
+    <h3 class="text-6xl h-12 text-white font-menobanner">{value}</h3>
+    <p class="content-center font-semibold text-large text-mcswf-gold">{metric}</p>
   </div>
+</div>
