432b5712d178784ca67902caab0884832fff9926
will <30413278+wcrum@users.noreply.github.com>
2024-04-22 20:02:53 -0600

    feat: accordion, update content image, isolate metrics (#40)
    * feat: + accordion, update content image, isolate metrics
    
    * fix: dynamic steps
    
    * fix: add apply now button
    
    * fix: update padding
    
    * Update prerequisites and added to career page.
    
    ---------
    
    Co-authored-by: Daniel Fullaway <dfullaway@danielmfullaway.com>

--- src/components/sections/Mission.svelte
+++ src/components/sections/Mission.svelte
@@ -1,22 +1,11 @@
 <script>
-    import Hero from '../elements/Hero.svelte';
-    import MetricCard from '../cards/MetricCard.svelte';
-  </script>
+  import Hero from '../elements/Hero.svelte';
+  import MetricCard from '../cards/MetricCard.svelte';
+  import MissionMetrics from '../elements/MissionMetrics.svelte'
+</script>
   
-  <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pb-10">
-    <!-- Title -->
-    <Hero linkText='LEARN MORE' padding="md:pt-24 pt-16 pb-12" title='OUR MISSION' content='We provide mission-specific software development, data analytics, and artificial intelligence operations capabilities to MIG commanders to close joint, coalition, and naval kill webs. These capabilities enhance Marine commanders decision advantage across the full spectrum of military operations.'></Hero>
-  
-    <div class="grid grid-cols-1 gap-8 p-10 sm:grid-cols-2 lg:grid-cols-4 md:gap-12">
-      {#each [
-        { icon: '/icons/icon homepage marines trained.svg', value: '4', metric: 'Marines Trained' },
-        { icon: '/icons/icon homepage applications deployed.svg', value: '4', metric: 'Applications Deployed' },
-        { icon: '/icons/icon homepage units using.svg', value: '16', metric: 'Units Using MCSWF Applications' },
-        { icon: '/icons/icon homepage average days.svg', value: '81', metric: 'Average Days 0-1 MVP' }
-      ] as { icon, value, metric }, index}
-        <div class="text-center border-white border-2 rounded-lg p-4" key={index}>
-          <MetricCard icon={icon} value={value} metric={metric} />
-        </div>
-      {/each}
-    </div>
-  </div>
+<div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pb-10">
+  <!-- Title -->
+  <Hero linkText='LEARN MORE' padding="md:pt-24 pt-16 pb-12" title='OUR MISSION' content='We provide mission-specific software development, data analytics, and artificial intelligence operations capabilities to MIG commanders to close joint, coalition, and naval kill webs. These capabilities enhance Marine commanders decision advantage across the full spectrum of military operations.'></Hero>
+  <MissionMetrics />
+</div>
