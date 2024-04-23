5f4b2f85811c798e2867e433953c8981a1450157
Scott Pierce-Wrobel <scottpiercew@gmail.com>
2024-04-23 07:53:14 -0400

    linting button

--- src/components/buttons/ButtonCustom.svelte
+++ src/components/buttons/ButtonCustom.svelte
@@ -16,7 +16,7 @@
       'bg-mcswf-white-50 text-usmc-red hover:bg-zinc-200 hover:focus:ring-zinc-400/0 focus:ring-zinc-400/70 ',
     yellow:
       'bg-mcswf-yellow text-usmc-red hover:bg-amber-400 hover:focus:ring-amber-600/0 focus:ring-amber-600/70 ',
-    red: 'bg-mcswf-red text-mcswf-white-50 hover:bg-red-900 hover:focus:ring-red-950/0 focus:ring-red-950/70 ',
+    red: 'bg-mcswf-red text-mcswf-white-50 hover:bg-red-900 hover:focus:ring-red-950/0 focus:ring-red-950/70 '
   }
 </script>
 
