835f2b037fa390fdf56f5d3622c291897e3487ae
Scott Pierce-Wrobel <scottpiercew@gmail.com>
2024-04-23 07:50:10 -0400

    adds eslint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import globals from 'globals'
+
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginJs from '@eslint/js'
+import eslintPluginSvelte from 'eslint-plugin-svelte'
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+})
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  ...compat.extends('standard'),
+  ...eslintPluginSvelte.configs['flat/recommended'],
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // 'svelte/rule-name': 'error'
+    },
+  },
+]
