97a0471b134afe0d9c47f900ec0fabeef295f3b0
Sean Sullivan <seanbsullivan@gmail.com>
2024-04-25 23:09:17 -0400

    create new Button component usable directly from astro

---
+++ src/components/buttons/ButtonCustomFromAstro.svelte
@@ -0,0 +1,15 @@
+<script>
+  import ButtonCustom from '../buttons/ButtonCustom.svelte'
+
+  export let color = 'white' // white, yellow, red
+  export let size = 'md' // xs, sm, md, lg, xl
+  export let link = ''
+  export let linkText = ''
+  export let customClasses = ''
+  export let disabled = false
+  export let handleClick = null
+</script>
+
+<ButtonCustom {color} {size} {customClasses} {link} {disabled} {handleClick}>
+  {linkText}
+</ButtonCustom>
