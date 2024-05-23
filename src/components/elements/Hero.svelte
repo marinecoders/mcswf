3036a7d1ec88a05fd4f65fcef7660ffb7f0947b4
Daniel Fullaway <dfullaway@danielmfullaway.com>
2024-05-22 22:14:28 -0700

    Adjust hero to be left aligned in mobile. Added image gradient and rounding. Button centered. Edited cell phone image.

--- src/components/elements/Hero.svelte
+++ src/components/elements/Hero.svelte
@@ -12,22 +12,22 @@
 
 <div class={'max-w-[85rem] mx-auto px-4 pt-inherit ' + padding}>
   <!-- Title -->
-  <div class="max-w-6xl mx-auto mt-5 text-center">
-    <h1 class="block font-bold text-xl md:text-[40px] {titleClasses}">
+  <div class="max-w-6xl mx-auto mt-5 text-left lg:text-center">
+    <h1 class="block font-bold text-xl lg:text-[40px] {titleClasses}">
       {title}
     </h1>
   </div>
   <!-- End Title -->
 
-  <div class="max-w-3xl mx-auto mt-5 text-center">
-    <p id="content" class="text-white md:text-2xl {contentClasses}">
+  <div class="max-w-3xl mx-auto mt-5 text-left lg:text-center">
+    <p id="content" class="text-white lg:text-2xl {contentClasses}">
       {content}
     </p>
   </div>
 
   <!-- Buttons -->
   {#if link}
-    <div class="grid w-full gap-3 mt-8 sm:inline-flex sm:justify-center">
+    <div class="grid w-full gap-3 mt-8 inline-flex justify-center">
       <ButtonCustom color="white" size="xl" customClasses="w-64" {link}>
         {linkText}
       </ButtonCustom>
