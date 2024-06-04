88907e22f4e4bc12fe41f266e7cd9fc7ad6ba4cc
Daniel Fullaway <32427337+dfullaway@users.noreply.github.com>
2024-06-04 10:56:58 -0500

    Add breakpoint width to fix overlapping metric cards. (#62)
    * Add breakpoint width to fix overlapping metric cards.
    
    * Updates font size of metric card text
    
    ---------
    
    Co-authored-by: Scott Pierce-Wrobel <scottpiercew@gmail.com>

--- src/components/cards/MetricCard.svelte
+++ src/components/cards/MetricCard.svelte
@@ -3,11 +3,11 @@
   export let value;
   export let metric;
 </script>
-  
-<div class="rounded-lg flex h-72 w-56 border-2 p-4 border-white">
+
+<div class="rounded-lg flex h-72 w-56 lg:w-48 xl:w-56 border-2 p-4 border-white">
   <div class="w-7/8 m-auto align-middle grid gap-4">
     <img class="mx-auto max-h-24" src={icon} alt="Description" />
     <h3 class="text-6xl h-12 text-white font-menobanner">{value}</h3>
-    <p class="content-center font-semibold text-large text-mcswf-gold">{metric}</p>
+    <p class="content-center font-semibold text-lg text-mcswf-gold">{metric}</p>
   </div>
 </div>
