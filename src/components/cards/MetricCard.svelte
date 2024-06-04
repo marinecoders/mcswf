c861dc4b4c83587d8f651f2c4b9a5c5802d7ed14
Daniel Fullaway <dfullaway@danielmfullaway.com>
2024-06-04 09:56:16 -0500

    Add breakpoint width to fix overlapping metric cards.

--- src/components/cards/MetricCard.svelte
+++ src/components/cards/MetricCard.svelte
@@ -4,7 +4,7 @@
   export let metric;
 </script>
   
-<div class="rounded-lg flex h-72 w-56 border-2 p-4 border-white">
+<div class="rounded-lg flex h-72 w-56 lg:w-48 xl:w-56 border-2 p-4 border-white">
   <div class="w-7/8 m-auto align-middle grid gap-4">
     <img class="mx-auto max-h-24" src={icon} alt="Description" />
     <h3 class="text-6xl h-12 text-white font-menobanner">{value}</h3>
