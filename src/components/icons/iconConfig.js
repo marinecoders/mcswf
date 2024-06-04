d7fae52a68d04b97208a78f072ea42f8a894a45b
Scott Pierce-Wrobel <scottpiercew@gmail.com>
2024-06-03 23:59:13 -0500

    Scottpw/social icons setup (#60)
    *Removes Twitter link
    *Creates icon configuration files for consistent icon usage
    *Updates prettier and eslint config to standardize formatting

---
+++ src/components/icons/iconConfig.js
@@ -0,0 +1,7 @@
+export const sizes = {
+  xs: 'w-3 h-3', // 12px x 12px
+  sm: 'w-4 h-4', // 16px x 16px
+  md: 'w-6 h-6', // 24px x 24px
+  lg: 'w-10 h-10', // 40px x 40px
+  xl: 'w-14 h-14', // 56px x 56px
+};
