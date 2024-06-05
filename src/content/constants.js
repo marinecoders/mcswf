d7564b3bc782a7d1563e686b1726e30fdd88ad64
Sean Sullivan <102331660+seanbsullivan@users.noreply.github.com>
2024-06-05 17:03:29 -0400

    Fix About page (#57)
    * swapped in new images from figma
    
    * remove old images
    
    * swap hero image with new one
    
    * adjust hero image position + add black gradient over image
    
    * crop images
    
    * update links
    
    * Corrected top image. Updated metric card margins.
    
    * updated button text to get in touch
    
    * Add MCSWF email link.
    
    Added MCSWF email to constants.
    
    * moves email subject to constants
    
    ---------
    
    Co-authored-by: Daniel Fullaway <dfullaway@danielmfullaway.com>
    Co-authored-by: Scott Pierce-Wrobel <scottpiercew@gmail.com>

--- src/content/constants.js
+++ src/content/constants.js
@@ -1,3 +1,5 @@
 export const externalLinks = {
   recruitingApplication: 'https://docs.google.com/forms/d/e/1FAIpQLSeJBKVSocqNje8_Tn7OUUZpELNDBJU65RUMeh_9bzvHa2bEzg/viewform',
+  mcswfEmail: 'MCSWF@usmc.mil',
+  mcswfEmailSubject: 'About MCSWF',
 };
