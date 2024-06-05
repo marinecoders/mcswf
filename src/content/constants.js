0849ade9c3734aa6380aeaedaa2b6ff7d6ed6f72
Daniel Fullaway <dfullaway@danielmfullaway.com>
2024-06-05 13:38:21 -0500

    Add MCSWF email link.
    Added MCSWF email to constants.

--- src/content/constants.js
+++ src/content/constants.js
@@ -1,3 +1,4 @@
 export const externalLinks = {
   recruitingApplication: 'https://docs.google.com/forms/d/e/1FAIpQLSeJBKVSocqNje8_Tn7OUUZpELNDBJU65RUMeh_9bzvHa2bEzg/viewform',
+  mcswfEmail: 'mailto:MCSWF@usmc.mil',
 };
