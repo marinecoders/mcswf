ff3cc42e522a40c7d0814f10eefcdb94f839ad07
Scott Pierce-Wrobel <scottpiercew@gmail.com>
2024-06-05 16:03:02 -0500

    moves email subject to constants

--- src/content/constants.js
+++ src/content/constants.js
@@ -1,4 +1,5 @@
 export const externalLinks = {
   recruitingApplication: 'https://docs.google.com/forms/d/e/1FAIpQLSeJBKVSocqNje8_Tn7OUUZpELNDBJU65RUMeh_9bzvHa2bEzg/viewform',
-  mcswfEmail: 'mailto:MCSWF@usmc.mil',
+  mcswfEmail: 'MCSWF@usmc.mil',
+  mcswfEmailSubject: 'About MCSWF',
 };
