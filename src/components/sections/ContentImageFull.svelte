09344db9ea163386e78989480bdb1cf775f2fc91
Daniel Fullaway <32427337+dfullaway@users.noreply.github.com>
2024-06-06 10:32:13 -0500

    Adjust padding. Left align mobile text. (#71)

--- src/components/sections/ContentImageFull.svelte
+++ src/components/sections/ContentImageFull.svelte
@@ -2,7 +2,7 @@
   import ButtonCustom from '../buttons/ButtonCustom.svelte';
 
   export let title;
-  export let titleClasses = 'text-mcswf-gold text-2xl font-bold md:text-3xl lg:text-4xl';
+  export let titleClasses = '';
   export let content = null;
   export let link = null;
   export let linkText = null;
@@ -13,7 +13,7 @@
 </script>
 
 <section class="">
-  <div class="items-center max-w-screen-xl gap-16 px-4 py-8 mx-auto lg:grid lg:grid-cols-2 md:py-8 md:px-6">
+  <div class="items-center max-w-screen-xl gap-16 px-4 py-8 mx-auto grid grid-cols-1 justify-center lg:grid-cols-2 md:py-8 md:px-6">
     {#if flipped}
       <div class="flex justify-center">
         <img class="w-3/4 rounded-lg aspect-square object-cover {imageClasses}" src={image} alt={imageAltText} />
@@ -24,8 +24,8 @@
         <img class="w-3/4 rounded-lg object-cover aspect-square {imageClasses}" src={image} alt={imageAltText} />
       </div>
     {/if}
-    <div class="font-light text-white sm:text-lg">
-      <h2 class="mb-4 tracking-tight text-center {titleClasses}">
+    <div class="font-light text-white sm:text-lg w-3/4 lg:w-full place-self-center">
+      <h2 class="mb-4 mt-6 lg:mt-0 tracking-tight lg:text-center  text-mcswf-gold text-2xl font-bold md:text-3xl lg:text-4xl {titleClasses}">
         {title}
       </h2>
       <div class="flex justify-center">
@@ -36,7 +36,7 @@
             {/each}
           </ul>
         {:else}
-          <p class="mb-4 text-left lg:text-center w-3/4 lg:w-full whitespace-pre-line text-base md:text-[21px]">
+          <p class="mb-4 text-left lg:text-center  whitespace-pre-line text-base md:text-[21px]">
             {content}
           </p>
         {/if}
