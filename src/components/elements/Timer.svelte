fee903a7c003e7d105fc16df7e61b847f82df8ae
Daniel Fullaway <dfullaway@danielmfullaway.com>
2024-06-07 13:34:37 -0500

    Tweaks to timer.

--- src/components/elements/Timer.svelte
+++ src/components/elements/Timer.svelte
@@ -33,13 +33,13 @@
     });
   </script>
   
-  <div class="grid w-full grid-cols-1 gap-3 mt-8 md:grid-cols-4 space-x-10 sm:inline-flex sm:justify-center">
-    <div class="hidden my-auto bg-red-600 w-60 md:block">
+  <div class="grid w-full grid-cols-1 gap-3 mt-8 lg:grid-cols-4 lg:inline-flex sm:justify-between">
+    <div class="hidden my-auto bg-red-600 w-42 lg:w-60 md:block">
       <hr class="h-[1px] bg-white border-none" />
     </div>
     {#each [days, hours, minutes, seconds] as time, index}
       <div class="items-center text-center" key={index}>
-        <div class="mt-2 sm:mt-4">
+        <div class="mt-8 sm:mt-4">
           <h3 class="text-8xl md:text-6xl font-extrabold font-menobanner">{time}</h3>
           <p class="font-bold text-xl md:text-base text-mcswf-gold">
             {index === 0 ? 'DAYS' : index === 1 ? 'HOURS' : index === 2 ? 'MINUTES' : 'SECONDS'}
@@ -47,7 +47,7 @@
         </div>
       </div>
     {/each}
-    <div class="hidden my-auto w-60 md:block">
+    <div class="hidden my-auto w-42 lg:w-60 md:block">
       <hr />
     </div>
   </div>
