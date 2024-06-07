925af127f16fa2b6c6cfa314949f5a6adf634b8a
Scott Pierce-Wrobel <scottpiercew@gmail.com>
2024-06-06 19:47:16 -0500

    updates footer alignment (#75)

--- src/components/FooterFull.svelte
+++ src/components/FooterFull.svelte
@@ -1,7 +1,7 @@
 <script>
   import Icon from '@iconify/svelte';
   import { LinkedIn } from '@icons/iconExports';
-
+  import { externalLinks } from '@content/constants';
   export let background = '';
   export let rounded = true;
 </script>
@@ -10,7 +10,7 @@
   <footer class="pb-0 pt-10 lg:pb-2 blue-radial-gradient" class:rounded-t-3xl={rounded}>
     <!-- STANDARD FOOTER -->
     <div class="hidden lg:block container w-4/5 max-w-screen-xl pb-32 mx-auto lg:py-8">
-      <div class="flex justify-around p-4 border-b">
+      <div class="flex justify-around py-4 border-b">
         <div class="flex w-2/5 align-middle">
           <a href="/" class="flex items-center mb-4 space-x-3 sm:mb-0 rtl:space-x-reverse">
             <img style="fill: white;" src="/mcswf-badge.svg" class="h-16 text-white" alt="MCSWF Logo" />
@@ -19,42 +19,44 @@
         </div>
         <div class="w-3/5">
           <h1 class="text-lg font-bold">VISION</h1>
-          <p>
+          <p class="font-light">
             Create a world class Marine-led software development capability enabling Marine Corps modernization efforts by developing applications for commanders at the speed of operations, increasing
             lethality and readiness.
           </p>
         </div>
       </div>
-      <div class="flex justify-around p-4 align-middle border-b">
+      <div class="flex justify-around py-4 align-middle border-b">
         <div class="w-2/5">
           <a href="/" class="flex items-center mb-4 space-x-3 sm:mb-0 rtl:space-x-reverse">
             <img style="fill: white;" src="/ega-full.svg" class="h-12 text-white" alt="MCSWF Logo" />
           </a>
         </div>
         <div class="w-3/5">
-          <p>
-            Product of The Marine Corps Software Factory, which proudly sits within the <span class="underline text-mcswf-gold">Deputy Commandant for Information.</span>
+          <p class="font-light">
+            Product of The Marine Corps Software Factory, which proudly sits within the <a href={externalLinks.deputyCommandantForInformation} class="underline text-mcswf-gold"
+              >Deputy Commandant for Information</a
+            >.
           </p>
         </div>
       </div>
-      <div class="flex justify-around p-4 border-b">
+      <div class="flex justify-around items-center py-4 mb-8 border-b">
         <div class="w-2/5">
-          <h1 class="mb-2">GET IN TOUCH</h1>
+          <h1 class="mb-2 font-light text-xs">GET IN TOUCH</h1>
           <div class="flex justify-start mb-4">
-            <a href="https://linkedin.com" class="mr-4" target="_blank" rel="noopener noreferrer">
+            <a href={externalLinks.social.linkedIn} class="mr-4" target="_blank" rel="noopener noreferrer">
               <LinkedIn size="lg" primary="fill-linkedin-blue" secondary="fill-white" class="rounded-sm" />
             </a>
           </div>
         </div>
-        <div class="w-3/5 align-middle">
+        <div class="w-3/5 font-light">
           <p>Interested in partnering with the Marine Corps Software Factory?</p>
           <p>
-            Email us at <a href="mailto:mcswf@usmc.mil" class="underline text-mcswf-gold">mcswf@usmc.mil</a>
+            Email us at <a href={`mailto:${externalLinks.mcswfEmail}?subject=${externalLinks.mcswfEmailSubject}`} class="underline text-mcswf-gold">{externalLinks.mcswfEmail}</a>
           </p>
         </div>
       </div>
-      <div class="flex justify-center pt-4 text-sm font-light">
-        Proudly developed at<a class="underline ps-1 text-mcswf-gold" href="https://www.marforres.marines.mil/MIU/">Marine Innovation Unit</a>
+      <div class="text-transparent">
+        Proudly developed at<a class="underline ps-1" href="https://www.marforres.marines.mil/MIU/">Marine Innovation Unit</a>
       </div>
     </div>
 
