fe34196dc25df9e8b6e111089da39be3fcaae411
Scott Pierce-Wrobel <scottpiercew@gmail.com>
2024-06-07 15:23:08 -0500

    creates mobile site navigation header

--- src/components/SiteNavHeader.svelte
+++ src/components/SiteNavHeader.svelte
@@ -3,9 +3,10 @@
   import Hamburger from '@icons/Hamburger.svelte'; // Ensure this path is correct
   import ForwardArrow from '@icons/ForwardArrow.svelte';
   import { slide } from 'svelte/transition';
-  import { quintOut } from 'svelte/easing';
   import { onMount } from 'svelte';
   import { externalLinks } from '@content/constants';
+  import { AccordionItem, Accordion } from 'flowbite-svelte';
+  import Icon from '@iconify/svelte';
 
   let isOpen = false;
 
@@ -62,6 +63,14 @@
       document.removeEventListener('click', handleClickOutside);
     };
   });
+
+  const fadeSlide = (node, options) => {
+    const slideTrans = slide(node, options);
+    return {
+      duration: options.duration,
+      css: (t) => `${slideTrans.css(t)} opacity: ${t}`,
+    };
+  };
 </script>
 
 <header
@@ -79,7 +88,7 @@
         <ButtonCustom
           color="white"
           size="md"
-          class="px-2 py-1 text-xs sm:px-4 sm:text-base ml-auto"
+          class="px-2 py-1 text-xs sm:px-4 sm:text-base ml-auto hidden sm:block"
           link={externalLinks.recruitingApplication}>GET IN TOUCH</ButtonCustom>
       {/if}
       <div class="self-center scale-50">
@@ -90,9 +99,15 @@
     </div>
     {#if isOpen}
       <div
-        class="absolute bg-custom-gradient-blue-header px-16 w-full"
-        transition:slide={{ duration: 300, easing: quintOut, axis: 'y' }}>
-        <div class="grid grid-cols-4 py-4 gap-1 sm:gap-2 lg:gap-4 md:pr-12 lg:pr-32 xl:pr-64">
+        class="absolute bg-custom-gradient-blue-header px-16 w-full sm:h-auto h-screen"
+        transition:fadeSlide={{ duration: 200 }}>
+        <div class="flex justify-center">
+          <a
+            href={externalLinks.recruitingApplication}
+            class="m-auto sm:hidden underline py-4 text-mcswf-gold">APPLY NOW</a>
+        </div>
+
+        <div class="hidden sm:grid sm:grid-cols-4 py-4 gap-4 lg:gap-4 md:pr-12 lg:pr-32 xl:pr-64">
           {#each dropdownLinks as link}
             <div class="col-span-1 tracking-wide">
               <div class="text-lg font-bold bg-gradient-to-r from-white via-slate-50 to-slate-100 inline-block text-transparent bg-clip-text">{link.text}</div>
@@ -112,6 +127,43 @@
               {/if}
             </div>
           {/each}
+        </div>
+        <div class="block sm:hidden py-10 text-white fill-whith">
+          <Accordion flush>
+            {#each dropdownLinks as link}
+              <AccordionItem
+                borderBottomClass="none"
+                paddingFlush="none">
+                <span
+                  slot="header"
+                  class="text-white text-[20px] py-2 hover:cursor-pointer hover:opacity-85">{link.text}</span>
+                <Icon
+                  slot="arrowdown"
+                  class="text-white"
+                  icon="mdi:plus" />
+                <div
+                  slot="arrowup"
+                  class="text-white font-bold mr-1">
+                  -
+                </div>
+
+                {#if link.subLinks}
+                  <ul class="text-base">
+                    {#each link.subLinks as subLink}
+                      <li class="p-1 hover:cursor-pointer">
+                        <a
+                          href={subLink.url}
+                          class="hover:text-mcswf-gold hover:underline underline-offset-4 mx-6 text-white"
+                          >{subLink.text}
+                          <ForwardArrow />
+                        </a>
+                      </li>
+                    {/each}
+                  </ul>
+                {/if}
+              </AccordionItem>
+            {/each}
+          </Accordion>
         </div>
       </div>
     {/if}
