1315ff9bd81e78041e5bb3810e40cf1e8c52228a
Daniel Fullaway <dfullaway@danielmfullaway.com>
2024-06-07 15:43:00 -0500

    WIP. Scrolling product cards.

--- src/components/cards/MCCard.svelte
+++ src/components/cards/MCCard.svelte
@@ -1,18 +1,22 @@
 <script>
     export let title;
+    export let shortDescription;
     export let link;
-    export let imageSrc;
+    export let position;
   </script>
   
-  <div class="rounded-lg w-full max-w-sm bg-slate-300 border border-gray-200 shadow-lg bg-slate-900 border-slate-700">
-    <div class="flex flex-col items-center pb-10 text-center">
-      <img class="m-4 w-24 h-24 mb-3 rounded-full shadow-lg" src={imageSrc}/>
-      <h5 class="mb-1 font-extrabold text-4xl text-gray-900 text-white">
-        {title}
-      </h5>
-      <div class="flex font-light mt-2 space-x-3">
+  <div class="rounded-lg w-52 h-72 shadow-lg bg-slate-900 snap-center">
+    <div class="flex flex-col items-center pb-10 text-center m-6">
+      <div class="flex justify-start self-start pb-6">
+        <span class="text-left font-menobanner font-semibold text-7xl">{position}</span>
+        <h5 class="mb-1 font-extrabold text-2xl align-middle text-gray-900 text-white pl-4 self-center">
+          {title}
+        </h5>
+      </div>
+      <div class="text-base text-left">{shortDescription}</div>
+      <div class="self-start flex mt-2 space-x-3">
         {#if link}
-          <a href={link} class="inline-flex items-center">LEARN MORE &gt;</a>
+          <a href={link} class="inline-flex items-center text-mcswf-gold text-xl font-bold underline underline-offset-4 text-left">learn more</a>
         {/if}
       </div>
     </div>
--- src/components/sections/Products.svelte
+++ src/components/sections/Products.svelte
@@ -13,22 +13,17 @@
 </script>
 
 <div class="flex flex-row justify-center pt-12 pb-4">
-  <h1 class="text-center font-bold text-3xl">
+  <h1 class="text-center font-bold text-3xl mx-6">
     BUILDING PRODUCTS THAT MAKE A DIFFERENCE
   </h1>
 </div>
-<div class="flex flex-row">
-  <div class="flex-initial basis-1/12"></div>
-  <div class="basis-10/12 flex flex-wrap">
-    {#each products as product}
-      <div class="flex pt-3 justify-center w-full sm:w-full md:w-full lg:w-1/2 xl:w-1/2">
-        <Card
-          title={product.data.product}
-          link={expandLink(product.id)}
-          imageSrc="https://dummyimage.com/25x25/fff/aaa"
-        />
-      </div>
-    {/each}
-  </div>
-  <div class="basis-1/12"></div>
+<div class="grid grid-flow-col justify-around gap-2 overflow-x-auto snap-x h-fit">
+  {#each products as product}
+      <Card
+        title={product.data.product}
+        link={expandLink(product.id)}
+        shortDescription={product.data.shortDescription}
+        position={product.data.order}
+      />
+  {/each}
 </div>
