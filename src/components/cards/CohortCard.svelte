289c25f3309be655acd8f3f032260abeb9cec626
Daniel Fullaway <32427337+dfullaway@users.noreply.github.com>
2024-06-06 19:02:57 -0500

    #187546527 Rework. (#74)
    Left align "Ready to Apply".
    Increase cohort card size.
    Increase width of apply now button on cohort cards.

--- src/components/cards/CohortCard.svelte
+++ src/components/cards/CohortCard.svelte
@@ -8,8 +8,8 @@
   export let link;
 </script>
 
-<div class=" w-64 p-4 border rounded-lg shadow-md bg-white">
-  <div class="flex justify-end">
+<div class="w-10/12 lg:w-64 p-4 border rounded-lg shadow-md bg-white">
+  <div class="flex justify-end pb-4">
     <a href={link}>
       <Icon class="text-black text-2xl" icon="mdi:share-variant-outline"></Icon>
     </a>
@@ -17,11 +17,11 @@
   <div class="text-center pb-4">
     <h2 class="text-3xl font-bold text-black">{title}</h2>
   </div>
-  <div class="text-center">
+  <div class="text-center py-4">
     <h2 class="text-lg font-bold text-black">{startDate}</h2>
   </div>
-  <p class="text-gray-700 text-base pt-1 pb-4 text-center">{content}</p>
-  <div class="flex justify-center" href={link}>
-    <ButtonCustom color="red" size="lg" customClasses="w-52" {link}>APPLY NOW</ButtonCustom>
+  <p class="text-gray-700 text-base pt-1 pb-6 text-center">{content}</p>
+  <div class="flex justify-center pb-6">
+    <ButtonCustom color="red" size="lg" class="w-full lg:w-auto" {link}>APPLY NOW</ButtonCustom>
   </div>
 </div>
