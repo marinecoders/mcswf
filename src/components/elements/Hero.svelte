a5c2d230c9da3f7caf26c21ab03a5afb8601584f
Scott Pierce-Wrobel <scottpiercew@gmail.com>
2024-06-12 10:57:18 -0400

    Updates 404 page styles to match figma and adds rounded bottom hero borders

--- src/components/elements/Hero.svelte
+++ src/components/elements/Hero.svelte
@@ -13,17 +13,17 @@
   export let backgroundImg = '';
 
   const colorSpecificClasses = {
-    white: 'from-white via-slate-50 to-slate-100',
+    white: 'from-white via-slate-50 to-slate-200',
     yellow: 'from-mcswf-gold via-yellow-300 to-mcswf-gold',
   };
 </script>
 
 <div
   style={`background-image: url("${backgroundImg}");`}
-  class={twMerge('relative z-20 w-full bg-cover bg-center bg-no-repeat rounded-b-2xl md:rounded-none h-[400px] md:h-[550px]', $$props.class)}>
-  <div class="absolute inset-0 bg-stone-900 opacity-25 rounded-b-2xl md:rounded-none"></div>
+  class={twMerge('relative z-20 w-full bg-cover bg-center bg-no-repeat rounded-b-2xl h-[400px] md:h-[550px]', $$props.class)}>
+  <div class="absolute inset-0 bg-stone-900 opacity-25 rounded-b-2xl"></div>
   <div
-    class="relative h-[400px] md:h-[550px] bg-gradient-to-t from-stone-950 flex flex-col gap-6 md:gap-14 items-stretch md:items-center justify-end md:justify-center text-left md:text-center mx-auto rounded-b-2xl md:rounded-none tracking-wider px-8 pb-12">
+    class="relative h-[400px] md:h-[550px] bg-gradient-to-t from-stone-950 flex flex-col gap-6 md:gap-14 items-stretch md:items-center justify-end md:justify-center text-left md:text-center mx-auto rounded-b-2xl tracking-wider px-8 pb-12">
     <!-- Title -->
     <h1 class={twMerge('text-left md:text-center font-bold text-[32px] md:text-[40px] bg-gradient-to-r text-transparent bg-clip-text', titleClasses, colorSpecificClasses[titleColor])}>
       {#if !title}
