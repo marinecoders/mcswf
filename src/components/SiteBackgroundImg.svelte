021530b1f14a6b32e4380c0fc1cd6a63f2ba30b7
Scott Pierce-Wrobel <scottpiercew@gmail.com>
2024-06-13 18:55:11 -0400

    Formats contact us pages (#96)

--- src/components/SiteBackgroundImg.svelte
+++ src/components/SiteBackgroundImg.svelte
@@ -2,6 +2,8 @@
   import Ega from '@components/logos/Ega.svelte';
 </script>
 
-<div class="fixed left-0 -z-10 flex justify-center w-full align-middle top-32 md:top-48 h-full -translate-x-8 md:-translate-x-16">
-  <Ega />
+<div class="absolute top-0 w-screen h-full bg-custom-gradient-red-banner -z-20">
+  <div class="fixed left-0 -z-10 flex justify-center w-full align-middle top-32 md:top-48 h-full -translate-x-8 md:-translate-x-16">
+    <Ega />
+  </div>
 </div>
